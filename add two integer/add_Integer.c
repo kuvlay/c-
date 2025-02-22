e22bbb4bff8447c2d9b87b578d81aacacf98dd75
kuvlay naaman <nkuvlay@gmail.com>
2025-02-23 01:25:57 +0530

    updated program

---
+++ add two integer/add_Integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d" ,&a);
+    printf("enter the first integer %d",a);
+    scanf("%d", &b);
+    printf("enter the second integer %d",b);
+
+     c =a+b;
+    
+    printf("add two integer is %d ",c);
+
+    return 0;
+
+}
